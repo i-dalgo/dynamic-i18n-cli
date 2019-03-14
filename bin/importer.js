759e733aefeba64da93a5e00e8d17c5d94dabce0
clovis <clovis.maniguet@idalgo.fr>
2019-03-14 14:26:09 +0100

    added some logs for import process

--- bin/importer.js
+++ bin/importer.js
@@ -1,10 +1,16 @@
 'use strict';
+const shell = require('shelljs');
 const unflatten = require('flat').unflatten;
 const axios = require('axios');
 const writeFile = require('./utils').writeFile;
 const https = require('https');
 
 module.exports = async (apiUrl, localesPath = './src/lang/') => {
+  const start = new Date()
+  let status = true
+  shell.echo(`
+  ✨  Importing locales from "${apiUrl}"
+  `);
   const agent = new https.Agent({
     rejectUnauthorized: false
   });
@@ -18,6 +24,24 @@ module.exports = async (apiUrl, localesPath = './src/lang/') => {
       file[row.key] = row[lang];
     });
     file = unflatten(file);
-    writeFile(`${localesPath}${lang}.json`, JSON.stringify(file));
+    const path = `${localesPath}${lang}.json`;
+    const fileStatus = writeFile(path, JSON.stringify(file));
+    if (!fileStatus) {
+      status = false
+      shell.echo(`  ❌  Can't create ${path}`);
+    } else {
+      shell.echo(`  ✍  Created ${path}`);
+    }
   });
+
+  const end = new Date() - start
+
+  // Something wrong happen => returning an error
+  if (!status) process.exit(1);
+
+  // Everything works
+  shell.echo(`
+  ✨  Done ! ${languages.length} file(s) created in ${end}ms !
+  `);
+  process.exit();
 }
